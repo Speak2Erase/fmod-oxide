0119206865c10b7c933d3adbf9fbc44ddf0e553e
Speak2Erase <lily@nowaffles.com>
2024-01-18 18:21:19 -0800

    Add more system methods

--- fmod-rs/src/studio/system.rs
+++ fmod-rs/src/studio/system.rs
@@ -198,6 +198,7 @@ impl From<System> for *mut FMOD_STUDIO_SYSTEM {
 unsafe impl Send for System {}
 unsafe impl Sync for System {}
 
+// todo: could we solve this with an "owned" system and a shared system?
 impl System {
     /// A convenience function over [`SystemBuilder`] with sane defaults.
     ///
@@ -222,5 +223,35 @@ impl System {
     /// This function is not safe to be called at the same time across multiple threads.
     pub unsafe fn release(self) -> Result<()> {
         unsafe { FMOD_Studio_System_Release(self.inner) }.to_result()
+    }
+}
+
+impl System {
+    // todo: take &self or not?
+    /// Update the FMOD Studio System.
+    ///
+    /// When Studio is initialized in the default asynchronous processing mode this function submits all buffered commands for execution on the Studio Update thread for asynchronous processing.
+    /// This is a fast operation since the commands are not processed on the calling thread.
+    /// If Studio is initialized with [`InitFlags::DEFERRED_CALLBACKS`] then any deferred callbacks fired during any asynchronous updates since the last call to this function will be called.
+    /// If an error occurred during any asynchronous updates since the last call to this function then this function will return the error result.
+    ///
+    /// When Studio is initialized with [`InitFlags::SYNCHRONOUS_UPDATE`] queued commands will be processed immediately when calling this function, the scheduling and update logic for the Studio system are executed and all callbacks are fired.
+    /// This may block the calling thread for a substantial amount of time.
+    pub fn update(&self) -> Result<()> {
+        unsafe { FMOD_Studio_System_Update(self.inner) }.to_result()
+    }
+
+    /// This function blocks the calling thread until all pending commands have been executed and all non-blocking bank loads have been completed.
+    ///
+    /// This is equivalent to calling [`System::update`] and then sleeping until the asynchronous thread has finished executing all pending commands.
+    pub fn flush_commands(&self) -> Result<()> {
+        unsafe { FMOD_Studio_System_FlushCommands(self.inner) }.to_result()
+    }
+
+    /// Block until all sample loading and unloading has completed.
+    ///
+    /// This function may stall for a long time if other threads are continuing to issue calls to load and unload sample data, e.g. by creating new event instances.
+    pub fn flush_sample_loading(&self) -> Result<()> {
+        unsafe { FMOD_Studio_System_FlushSampleLoading(self.inner) }.to_result()
     }
 }
