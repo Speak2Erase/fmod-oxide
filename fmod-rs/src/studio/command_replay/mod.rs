879b5a44606d8ec484f5fcda76c10481251df226
Speak2Erase <lily@nowaffles.com>
2024-03-28 08:04:12 -0700

    Split command replay into two modules

--- fmod-rs/src/studio/command_replay/mod.rs
+++ fmod-rs/src/studio/command_replay/mod.rs
@@ -16,213 +16,25 @@
 // along with fmod-rs.  If not, see <http://www.gnu.org/licenses/>.
 
 use std::{
-    ffi::{c_char, c_float, c_int, CStr},
+    ffi::{c_float, c_int, CStr},
     marker::PhantomData,
     mem::MaybeUninit,
-    os::raw::c_void,
     sync::Arc,
 };
 
 use fmod_sys::*;
 
-use crate::{Guid, Shareable, UserdataTypes};
+use super::{CommandInfo, PlaybackState, System};
+use crate::UserdataTypes;
 
-use super::{
-    Bank, CommandInfo, EventDescription, EventInstance, LoadBankFlags, PlaybackState, System,
-};
+mod userdata;
+pub use userdata::*;
 
 #[derive(Debug, PartialEq, Eq)]
 #[repr(transparent)] // so we can transmute between types
 pub struct CommandReplay<U: UserdataTypes = ()> {
     pub(crate) inner: *mut FMOD_STUDIO_COMMANDREPLAY,
     _phantom: PhantomData<U>,
-}
-
-pub(crate) struct InternalUserdata<U: UserdataTypes> {
-    // we don't expose the callbacks at all so it's fine to just use a box
-    create_instance_callback: Option<Box<dyn CreateInstanceCallback<U>>>,
-    frame_callback: Option<Box<dyn FrameCallback<U>>>,
-    load_bank_callback: Option<Box<dyn LoadBankCallback<U>>>,
-    userdata: Option<Arc<U::CommandReplay>>,
-}
-
-pub struct CreateInstanceData<U: UserdataTypes> {
-    pub replay: CommandReplay<U>,
-    pub command_index: c_int,
-    pub event_description: EventDescription<U>,
-    pub userdata: Option<Arc<U::CommandReplay>>,
-}
-pub trait CreateInstanceCallback<U: UserdataTypes>:
-    Fn(CreateInstanceData<U>) -> Result<Option<EventInstance<U>>> + Shareable
-{
-}
-impl<T, U> CreateInstanceCallback<U> for T
-where
-    T: Fn(CreateInstanceData<U>) -> Result<Option<EventInstance<U>>> + Shareable,
-    U: UserdataTypes,
-{
-}
-
-pub struct FrameData<U: UserdataTypes> {
-    pub replay: CommandReplay<U>,
-    pub command_index: c_int,
-    pub current_time: c_float,
-    pub userdata: Option<Arc<U::CommandReplay>>,
-}
-pub trait FrameCallback<U: UserdataTypes>: Fn(FrameData<U>) -> Result<()> + Shareable {}
-impl<T, U> FrameCallback<U> for T
-where
-    T: Fn(FrameData<U>) -> Result<()> + Shareable,
-    U: UserdataTypes,
-{
-}
-
-pub struct LoadBankData<U: UserdataTypes> {
-    pub replay: CommandReplay<U>,
-    pub command_index: c_int,
-    pub bank_guid: Option<Guid>,
-    pub bank_filename: Option<&'static CStr>, // FIXME 'static wrong
-    pub load_flags: LoadBankFlags,
-    pub userdata: Option<Arc<U::CommandReplay>>,
-}
-pub trait LoadBankCallback<U: UserdataTypes>:
-    Fn(LoadBankData<U>) -> Result<Option<Bank<U>>> + Shareable
-{
-}
-impl<T, U> LoadBankCallback<U> for T
-where
-    T: Fn(LoadBankData<U>) -> Result<Option<Bank<U>>> + Shareable,
-    U: UserdataTypes,
-{
-}
-
-unsafe extern "C" fn internal_create_instance_callback<U: UserdataTypes>(
-    replay: *mut FMOD_STUDIO_COMMANDREPLAY,
-    command_index: c_int,
-    event_description: *mut FMOD_STUDIO_EVENTDESCRIPTION,
-    instance: *mut *mut FMOD_STUDIO_EVENTINSTANCE,
-    userdata: *mut c_void,
-) -> FMOD_RESULT {
-    // userdata should always be set
-    // when setting a callback by necessity userdata is set as we don't set any callbacks by default
-    #[cfg(debug_assertions)]
-    if userdata.is_null() {
-        eprintln!("commandreplay userdata is null. aborting");
-        std::process::abort();
-    }
-
-    // FIXME: handle unwinding panics
-    unsafe {
-        let internal_userdata = &mut *userdata.cast::<InternalUserdata<U>>();
-        // the callback should ALWAYS be set if this callback is set
-        let callback = internal_userdata
-            .create_instance_callback
-            .as_ref()
-            .unwrap_unchecked();
-        let data = CreateInstanceData {
-            replay: CommandReplay::from_ffi(replay),
-            command_index,
-            event_description: EventDescription::from_ffi(event_description),
-            userdata: internal_userdata.userdata.clone(),
-        };
-
-        let result = callback(data);
-
-        match result {
-            Ok(Some(i)) => {
-                *instance = i.inner;
-                FMOD_RESULT::FMOD_OK
-            }
-            Ok(None) => FMOD_RESULT::FMOD_OK,
-            Err(e) => e.code,
-        }
-    }
-}
-
-unsafe extern "C" fn internal_frame_callback<U: UserdataTypes>(
-    replay: *mut FMOD_STUDIO_COMMANDREPLAY,
-    command_index: c_int,
-    current_time: c_float,
-    userdata: *mut c_void,
-) -> FMOD_RESULT {
-    // userdata should always be set
-    // when setting a callback by necessity userdata is set as we don't set any callbacks by default
-    #[cfg(debug_assertions)]
-    if userdata.is_null() {
-        eprintln!("commandreplay userdata is null. aborting");
-        std::process::abort();
-    }
-
-    // FIXME: handle unwinding panics
-    unsafe {
-        let internal_userdata = &mut *userdata.cast::<InternalUserdata<U>>();
-        // the callback should ALWAYS be set if this callback is set
-        let callback = internal_userdata.frame_callback.as_ref().unwrap_unchecked();
-        let data = FrameData {
-            replay: CommandReplay::from_ffi(replay),
-            command_index,
-            current_time,
-            userdata: internal_userdata.userdata.clone(),
-        };
-
-        callback(data).into()
-    }
-}
-
-unsafe extern "C" fn internal_load_bank_callback<U: UserdataTypes>(
-    replay: *mut FMOD_STUDIO_COMMANDREPLAY,
-    command_index: c_int,
-    bank_guid: *const FMOD_GUID,
-    bank_filename: *const c_char,
-    load_flags: FMOD_STUDIO_LOAD_BANK_FLAGS,
-    bank: *mut *mut FMOD_STUDIO_BANK,
-    userdata: *mut c_void,
-) -> FMOD_RESULT {
-    // userdata should always be set
-    // when setting a callback by necessity userdata is set as we don't set any callbacks by default
-    #[cfg(debug_assertions)]
-    if userdata.is_null() {
-        eprintln!("commandreplay userdata is null. aborting");
-        std::process::abort();
-    }
-
-    // FIXME: handle unwinding panics
-    unsafe {
-        let internal_userdata = &mut *userdata.cast::<InternalUserdata<U>>();
-        // the callback should ALWAYS be set if this callback is set
-        let callback = internal_userdata
-            .load_bank_callback
-            .as_ref()
-            .unwrap_unchecked();
-
-        let data = LoadBankData {
-            replay: CommandReplay::from_ffi(replay),
-            command_index,
-            bank_guid: if bank_guid.is_null() {
-                None
-            } else {
-                Some(Guid::from(*bank_guid))
-            },
-            bank_filename: if bank_filename.is_null() {
-                None
-            } else {
-                Some(CStr::from_ptr(bank_filename))
-            },
-            load_flags: load_flags.into(),
-            userdata: internal_userdata.userdata.clone(),
-        };
-
-        let result = callback(data);
-
-        match result {
-            Ok(Some(b)) => {
-                *bank = b.inner;
-                FMOD_RESULT::FMOD_OK
-            }
-            Ok(None) => FMOD_RESULT::FMOD_OK,
-            Err(e) => e.code,
-        }
-    }
 }
 
 unsafe impl<U: UserdataTypes> Send for CommandReplay<U> {}
