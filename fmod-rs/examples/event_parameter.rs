0e087b9bcc9af0e4a2441b72cadd263a0b30fe01
Speak2Erase <lily@nowaffles.com>
2024-04-11 17:31:37 -0700

    Add more examples

---
+++ fmod-rs/examples/event_parameter.rs
@@ -0,0 +1,158 @@
+// Copyright (C) 2024 Lily Lyons
+//
+// This file is part of fmod-rs.
+//
+// fmod-rs is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// fmod-rs is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with fmod-rs.  If not, see <http://www.gnu.org/licenses/>.
+
+use crossterm::{
+    cursor::*,
+    event::{Event, KeyCode, KeyEvent},
+    execute,
+    terminal::*,
+};
+
+use lanyard::c;
+use std::io::Write;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let builder = unsafe {
+        // Safety: we call this before calling any other functions and only in main, so this is safe
+        fmod::studio::SystemBuilder::new()?
+    };
+
+    let system = builder.build(
+        1024,
+        fmod::studio::InitFlags::NORMAL,
+        fmod::InitFlags::NORMAL,
+    )?;
+
+    system.load_bank_file(
+        c!("fmod/api/studio/examples/media/Master.bank"),
+        fmod::studio::LoadBankFlags::NORMAL,
+    )?;
+    system.load_bank_file(
+        c!("fmod/api/studio/examples/media/Master.strings.bank"),
+        fmod::studio::LoadBankFlags::NORMAL,
+    )?;
+    system.load_bank_file(
+        c!("fmod/api/studio/examples/media/SFX.bank"),
+        fmod::studio::LoadBankFlags::NORMAL,
+    )?;
+
+    let event_description = system.get_event(c!("event:/Character/Player Footsteps"))?;
+
+    // Find the parameter once and then set by handle
+    // Or we can just find by name every time but by handle is more efficient if we are setting lots of parameters
+    let parameter_description =
+        event_description.get_parameter_description_by_name(c!("Surface"))?;
+    let surface_id = parameter_description.id;
+
+    let event_instance = event_description.create_instance()?;
+
+    let mut surface_parameter_value = 1.0;
+    event_instance.set_parameter_by_id(surface_id, surface_parameter_value, false)?;
+
+    // use alternate screen
+    let mut stdout = std::io::stdout();
+    execute!(stdout, EnterAlternateScreen, Hide)?;
+    crossterm::terminal::enable_raw_mode()?;
+
+    'main_loop: loop {
+        while crossterm::event::poll(std::time::Duration::from_micros(1000))? {
+            let event = crossterm::event::read()?;
+
+            let Event::Key(KeyEvent {
+                code: KeyCode::Char(character),
+                ..
+            }) = event
+            else {
+                continue;
+            };
+
+            match character {
+                ' ' => {
+                    event_instance.start()?;
+                }
+                '1' => {
+                    surface_parameter_value = parameter_description
+                        .minimum
+                        .max(surface_parameter_value - 1.0);
+                    event_instance.set_parameter_by_id(
+                        surface_id,
+                        surface_parameter_value,
+                        false,
+                    )?;
+                }
+                '2' => {
+                    surface_parameter_value = parameter_description
+                        .maximum
+                        .min(surface_parameter_value + 1.0);
+                    event_instance.set_parameter_by_id(
+                        surface_id,
+                        surface_parameter_value,
+                        false,
+                    )?;
+                }
+                'q' => {
+                    break 'main_loop;
+                }
+                _ => {}
+            }
+        }
+
+        system.update()?;
+
+        let (user_value, final_value) = event_instance.get_parameter_by_id(surface_id)?;
+
+        execute!(stdout, Clear(ClearType::All))?;
+
+        execute!(stdout, MoveTo(0, 0))?;
+        crossterm::terminal::disable_raw_mode()?;
+
+        stdout.write_all(b"==================================================\n")?;
+        stdout.write_all(b"Event Parameter Example.\n")?;
+        stdout.write_all(b"Adapted from the official FMOD example\n")?;
+        stdout.write_all(b"==================================================")?;
+        stdout.write_all(b"\n")?;
+        writeln!(
+            stdout,
+            "Surface Parameter = (user: {user_value}, final: {final_value})"
+        )?;
+        stdout.write_all(b"\n")?;
+
+        stdout.write_all(b"Surface Parameter:\n")?;
+        stdout.write_all(b"Press SPACE to play event\n")?;
+        stdout.write_all(b"Press 1 to decrease value\n")?;
+        stdout.write_all(b"Press 2 to increase value\n")?;
+        stdout.write_all(b"\n")?;
+        stdout.write_all(b"Press Q to quit\n")?;
+
+        crossterm::terminal::enable_raw_mode()?;
+
+        stdout.flush()?;
+
+        std::thread::sleep(std::time::Duration::from_millis(50));
+    }
+
+    // reset terminal
+    crossterm::terminal::disable_raw_mode()?;
+    execute!(stdout, LeaveAlternateScreen, Show)?;
+
+    unsafe {
+        // Safety: we don't use any fmod api calls after this, so this is ok
+        system.release()?;
+    }
+
+    Ok(())
+}
---
+++ fmod-rs/examples/objectpan.rs
@@ -0,0 +1,227 @@
+// Copyright (C) 2024 Lily Lyons
+//
+// This file is part of fmod-rs.
+//
+// fmod-rs is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// fmod-rs is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with fmod-rs.  If not, see <http://www.gnu.org/licenses/>.
+
+use crossterm::{
+    cursor::*,
+    event::{Event, KeyCode, KeyEvent},
+    execute,
+    terminal::*,
+};
+
+use lanyard::c;
+use std::io::Write;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let builder = unsafe {
+        // Safety: we call this before calling any other functions and only in main, so this is safe
+        fmod::studio::SystemBuilder::new()?
+    };
+
+    // The example Studio project is authored for 5.1 sound, so set up the system output mode to match
+    let system = builder
+        .software_format(0, fmod_sys::FMOD_SPEAKERMODE_FMOD_SPEAKERMODE_5POINT1, 0)?
+        // FIXME core system builder
+        // Due to a bug in WinSonic on Windows, FMOD initialization may fail on some machines.
+        // If you get the error "FMOD error 51 - Error initializing output device", try using
+        // a different output type such as FMOD_OUTPUTTYPE_AUTODETECT
+        .build(
+            1024,
+            fmod::studio::InitFlags::NORMAL,
+            fmod::InitFlags::NORMAL,
+        )?;
+
+    system.load_bank_file(
+        c!("fmod/api/studio/examples/media/Master.bank"),
+        fmod::studio::LoadBankFlags::NORMAL,
+    )?;
+    system.load_bank_file(
+        c!("fmod/api/studio/examples/media/Master.strings.bank"),
+        fmod::studio::LoadBankFlags::NORMAL,
+    )?;
+    system.load_bank_file(
+        c!("fmod/api/studio/examples/media/Music.bank"),
+        fmod::studio::LoadBankFlags::NORMAL,
+    )?;
+
+    let spatializer_description = system.get_event(c!("event:/Music/Radio Station"))?;
+    let spatializer_instance = spatializer_description.create_instance()?;
+    spatializer_instance.start()?;
+
+    let mut is_on_ground = false;
+    let mut use_listener_attenuation_position = false;
+    let mut t = 0.0f32;
+
+    // use alternate screen
+    let mut stdout = std::io::stdout();
+    execute!(stdout, EnterAlternateScreen, Hide)?;
+    crossterm::terminal::enable_raw_mode()?;
+
+    'main_loop: loop {
+        let (_, radio_frequency) = spatializer_instance.get_parameter_by_name(c!("Freq"))?;
+        let (_, spatializer) = spatializer_instance.get_parameter_by_name(c!("Spatializer"))?;
+
+        while crossterm::event::poll(std::time::Duration::from_micros(1000))? {
+            let event = crossterm::event::read()?;
+
+            let Event::Key(KeyEvent {
+                code: KeyCode::Char(character),
+                ..
+            }) = event
+            else {
+                continue;
+            };
+
+            match character {
+                '1' => {
+                    if radio_frequency == 3.0 {
+                        spatializer_instance.set_parameter_by_name(c!("Freq"), 0.0, false)?;
+                    } else {
+                        spatializer_instance.set_parameter_by_name(
+                            c!("Freq"),
+                            radio_frequency + 1.5,
+                            false,
+                        )?;
+                    }
+                }
+                '2' => {
+                    if spatializer == 1.0 {
+                        spatializer_instance.set_parameter_by_name(
+                            c!("Spatializer"),
+                            0.0,
+                            false,
+                        )?;
+                    } else {
+                        spatializer_instance.set_parameter_by_name(
+                            c!("Spatializer"),
+                            1.0,
+                            false,
+                        )?;
+                    }
+                }
+                '3' => {
+                    is_on_ground = !is_on_ground;
+                }
+                '4' => {
+                    use_listener_attenuation_position = !use_listener_attenuation_position;
+                }
+                'q' => {
+                    break 'main_loop;
+                }
+                _ => {}
+            }
+        }
+
+        let mut vec = fmod::Attributes3D::default();
+        vec.forward.z = 1.0;
+        vec.up.y = 1.0;
+
+        // Rotate sound in a circle
+        vec.position.x = t.sin() * 3.0;
+        vec.position.z = t.cos() * 3.0;
+        t += 0.03;
+
+        if is_on_ground {
+            vec.position.y = 0.0; // At ground level
+        } else {
+            vec.position.y = 5.0; // Up high
+        }
+
+        spatializer_instance.set_3d_attributes(vec)?;
+
+        let mut listener_vec = fmod::Attributes3D::default();
+        listener_vec.forward.z = 1.0;
+        listener_vec.up.y = 1.0;
+
+        let mut attenuation_pos = vec.position;
+        attenuation_pos.z -= 10.0;
+
+        system.set_listener_attributes(
+            0,
+            listener_vec,
+            use_listener_attenuation_position.then_some(attenuation_pos),
+        )?;
+
+        system.update()?;
+
+        let radio_string = match radio_frequency {
+            0.0 => "Rock",
+            1.5 => "Lo-fi",
+            _ => "Hip hop",
+        };
+        let spatial_string = if spatializer == 0.0 {
+            "Standard 3D Spatializer"
+        } else {
+            "Object Spatializer"
+        };
+
+        execute!(stdout, Clear(ClearType::All))?;
+
+        execute!(stdout, MoveTo(0, 0))?;
+        crossterm::terminal::disable_raw_mode()?;
+
+        stdout.write_all(b"==================================================\n")?;
+        stdout.write_all(b"Object Panning Example.\n")?;
+        stdout.write_all(b"Adapted from the official FMOD example\n")?;
+        stdout.write_all(b"==================================================")?;
+        stdout.write_all(b"\n")?;
+
+        writeln!(stdout, "Playing {radio_string} with the {spatial_string}.")?;
+        writeln!(
+            stdout,
+            "Radio is {}.",
+            if is_on_ground {
+                "on the ground"
+            } else {
+                "up in the air"
+            }
+        )?;
+
+        stdout.write_all(b"\n")?;
+        stdout.write_all(b"Press 1 to switch stations.\n")?;
+        stdout.write_all(b"Press 2 to switch spatializer.\n")?;
+        stdout.write_all(b"Press 3 to elevate the event instance.\n")?;
+        writeln!(
+            stdout,
+            "Press 4 to {} use of attenuation position.",
+            if use_listener_attenuation_position {
+                "disable"
+            } else {
+                "enable"
+            }
+        )?;
+        stdout.write_all(b"\n")?;
+
+        stdout.write_all(b"Press Q to quit\n")?;
+
+        crossterm::terminal::enable_raw_mode()?;
+
+        stdout.flush()?;
+
+        std::thread::sleep(std::time::Duration::from_millis(50));
+    }
+
+    // reset terminal
+    crossterm::terminal::disable_raw_mode()?;
+    execute!(stdout, LeaveAlternateScreen, Show)?;
+
+    unsafe {
+        // Safety: we don't use any fmod api calls after this, so this is ok
+        system.release()?;
+    }
+
+    Ok(())
+}
